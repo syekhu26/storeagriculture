4cdaa3c00a493bb7da7fa024c7882ecfd5e0f808
syekhuna <syekhuna.azwar@gmail.com>
2023-07-14 06:35:40 +0700

    update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -152,6 +152,10 @@ export default {
   },
   
   ssr:false,
+  generate: {
+    fallback:true
+  },
+
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
